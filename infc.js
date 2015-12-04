1ffdd2463722fd63c9049cb9fba6bd20e5a82bff
mavenlink <diclophis@gmail.com>
2015-12-03 23:07:09 -0800

    stream updates to browser

--- infc.js
+++ infc.js
@@ -6,6 +6,7 @@ var express = require('express'),
 
 var poppingConfig = null;
 var config = {};
+var messageCount = 0;
 
 process.argv.forEach(function (val, index, array) {
   //console.log(index + ': ' + val);
@@ -22,6 +23,8 @@ process.argv.forEach(function (val, index, array) {
 config['-p'] = config['-p'] || 3001;
 config['-f'] = config['-f'] || 'Watchfile';
 config['-h'] = config['-h'] || 'localhost';
+config['-c'] = null;
+
 console.log(config);
 
 fs.readFile(config['-f'] || 'Watchfile', 'utf8', function (err, data) {
@@ -42,6 +45,9 @@ fs.readFile(config['-f'] || 'Watchfile', 'utf8', function (err, data) {
 
         console.log('filename provided: ' + filename);
 
+        config['-c'].write('id: ' + messageCount + '\n');
+        config['-c'].write("data: " + filename + '\n\n'); // Note the extra newline
+        messageCount = messageCount + 1;
       } else {
         console.log('filename not provided');
       }
@@ -49,20 +55,51 @@ fs.readFile(config['-f'] || 'Watchfile', 'utf8', function (err, data) {
   });
 });
 
-
 app.use(function(req, res, next) {
   console.log(req.url);
   var isIndex = '/' === req.url;
+
+  var indexHtml = '<!DOCTYPE html><html><head><script>var source = new EventSource("/stream");source.addEventListener("message",function(e){console.log(e.data);},false);</script></head><body></body></html>'
   if (isIndex) {
-    res.send('<pre>' + "wtf" + '</pre>');
+    res.send(indexHtml);
   } else {
     next();
   }
 });
+
+app.get('/stream', function(req, res) {
+  // let request last as long as possible
+  //req.socket.setTimeout(Infinity);
+
+  if (config['-c']) {
+    config['-c'].end();
+  }
+
+  config['-c'] = res;
+
+
+  setInterval(function() {
+    //res.write('id: ' + messageCount + '\n');
+    //res.write("data: wtf" + '\n\n'); // Note the extra newline
+    //messageCount = messageCount + 1;
+  }, 1000);
+
+  //send headers for event-stream connection
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  res.write('\n');
+
+  // The 'close' event is fired when a user closes their browser window.
+  req.on("close", function() { });
+});
+
 
 // serve all files from public dir using built-in static file server
 app.use(express.static(path.dirname(config['-f']) + '/public'));
 
 var expressServer = app.listen(config['-p']);
 
-spawn('/usr/bin/open', ['http://' + config['-h'] + ':' + config['-p'] + '/']);
+//??? spawn('/usr/bin/open', ['http://' + config['-h'] + ':' + config['-p'] + '/']);
