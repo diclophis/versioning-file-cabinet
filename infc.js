f10ff64a07b36befe721a4edc2591dccc08daf20
mavenlink <diclophis@gmail.com>
2015-12-13 11:12:27 -0800

    better error detection

--- infc.js
+++ infc.js
@@ -45,6 +45,7 @@ var poppingConfig = null;
 var config = {};
 var messageCount = 0;
 
+
 process.argv.forEach(function (val, index, array) {
   if (null === poppingConfig) {
     poppingConfig = 'infc.js';
@@ -56,13 +57,13 @@ process.argv.forEach(function (val, index, array) {
   }
 });
 
+
 config['-p'] = config['-p'] || 3001;
 config['-f'] = config['-f'] || 'Folderfile';
 config['-h'] = config['-h'] || 'localhost';
 config['-c'] = null;
 config['publicDir'] = "public";
 
-//console.log(config);
 
 var promiseToSetResolvedFolderfilePath = function() {
   return new Promise(function(resolve, reject) {
@@ -73,6 +74,7 @@ var promiseToSetResolvedFolderfilePath = function() {
     });
   });
 };
+
 
 var promiseToCreateStaticFileCabinetDirectory = function() {
   return new Promise(function(resolve, reject) {
@@ -91,6 +93,7 @@ var promiseToCreateStaticFileCabinetDirectory = function() {
   });
 };
 
+
 var splitChars = function(txt, num) {
   var result = [];
   for (var i = 0; i < txt.length; i += num) {
@@ -99,9 +102,16 @@ var splitChars = function(txt, num) {
   return result;
 };
 
+
 var checksum = function(str, algorithm, encoding) {
   return crypto.createHash(algorithm || 'md5').update(str, 'utf8').digest(encoding || 'hex');
 };
+
+
+var stringPresent = function(str) {
+  return !(null === str || 0 === str.length);
+};
+
 
 var createClientInterface = function() {
   var source = new EventSource("/stream");
@@ -117,52 +127,37 @@ var createClientInterface = function() {
   }, false);
 };
 
+
 var promiseToWatchFilesFromFolders = function(validDirsToWatch, sendFile) {
   validDirsToWatch.forEach(function(validDirAndTildeScape, index, array) {
     var tildeScape = validDirAndTildeScape[0];
     var validDir = validDirAndTildeScape[1];
-    console.log('watching: ' + validDir);
     fs.watch(validDir, function (event, filename) {
-
-
-      console.log('event is: ' + event);
       if (filename) {
         var isChromeMeta = filename.startsWith('.com.google.Chrome');
         var isChromeDownload = filename.endsWith('.crdownload');
+        var isSwp = filename.endsWith('.swp');
 
         if (isChromeMeta
-            || isChromeDownload) { return; }
-
-        console.log('filename provided: ' + filename);
-
-        promiseToListAllFilesToSync().then(function(allFiles) {
-          allFiles.forEach(function(interestingFile) {
-            sendFile(interestingFile);
-          });
-        });
-        /*
-        if (config['-c']) {
-          //config['-c'].write('id: ' + messageCount + '\n');
-          //config['-c'].write("data: " + filename + '\n\n'); // Note the extra newline
-          //messageCount = messageCount + 1;
-        } else {
-          checkPathAndDo(filename); 
-        }
-        */
-        //if ("function" === typeof(sendFile)) {
-        //  sendFile(filename);
-        //}
-      } else {
-        console.log('filename not provided');
+            || isChromeDownload
+            || isSwp) { return; }
       }
+
+      promiseToListAllFilesToSync().then(function(allFiles) {
+        allFiles.forEach(function(interestingFile) {
+          var isInteresting = true;
+          if (filename && !interestingFile.endsWith(filename)) {
+            isInteresting = false;
+          }
+          if (isInteresting) {
+            sendFile(interestingFile);
+          }
+        });
+      });
     });
   });
 };
 
-
-var stringPresent = function(str) {
-  return !(null === str || 0 === str.length);
-};
 
 var promiseToListFolderfilesToSync = function() {
   return new Promise(function(resolve, reject) {
@@ -171,13 +166,13 @@ var promiseToListFolderfilesToSync = function() {
       resolve(data.split("\n").filter(function(element, index, array) {
         return stringPresent(element);
       }).map(function(element) {
-        //console.log(element.split("/"));
         var tildeScape = element.split("/").pop();
         return [tildeScape, element];
       }));
     });
   });
 };
+
 
 var loadIndexHtml = new Promise(function(resolve, reject) {
   fs.readFile(config['publicDir'] + '/index.html', function(err, data) {
@@ -191,7 +186,6 @@ var promiseToListAllFilesToSync = function() {
   var allFilesToSync = [];
   var allFileListingProcessPromises = [];
   return promiseToListFolderfilesToSync().then(function(foldersToSync) {
-    console.log(foldersToSync);
     foldersToSync.forEach(function(folderToSyncAndTildeScape) {
       var tildeScape = folderToSyncAndTildeScape[0];
       var folderToSync = folderToSyncAndTildeScape[1];
@@ -199,7 +193,6 @@ var promiseToListAllFilesToSync = function() {
       var fileListingProcessPromise = new Promise(function(res, rej) {
         var fileListingProcess = spawn("find", ["-f", folderToSync]);
         fileListingProcess.stdout.pipe(es.split()).pipe(es.map(function (data, cb) {
-          //console.log('Got the following message:', data);
           if (stringPresent(data) && folderToSync != data) {
             var canonFile = folderToSyncPrefix + data.replace(folderToSync, '');
             allFilesToSync.push(canonFile);
@@ -220,50 +213,17 @@ var promiseToListAllFilesToSync = function() {
 };
 
 
-
 var promiseToCopyFile = function(source, target, requested) {
-console.log(source, target, requested);
   return new Promise(function(resolve, reject) {
-        var mkdirAndCopyAndSymlink = "mkdir -p " + target + " && cp " + source + " " + target + " && mkdir -p " + path.dirname(requested) + " && ln -s " + target + "/" + path.basename(source) + " " + requested;
-        var fileListingProcess = spawn("sh", ["-c", mkdirAndCopyAndSymlink]);
-        fileListingProcess.stdout.pipe(es.split()).pipe(es.map(function (data, cb) {
-          //console.log('Got the following message:', data);
-          //if (stringPresent(data) && folderToSync != data) {
-          //  var canonFile = folderToSyncPrefix + data.replace(folderToSync, '');
-          //  allFilesToSync.push(canonFile);
-          //}
-          console.log("!!!", data);
-          cb(null);
-        }));
-        fileListingProcess.stdout.on('close', function(err) {
-          if (err) { reject(err); }
-          resolve();
-        });
-/*
-    var cbCalled = false;
-    var done = function(err) {
-      if (!cbCalled) {
-        cbCalled = true;
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      }
-    };
-    var rd = fs.createReadStream(source);
-    rd.on("error", function(err) {
-      done(err);
+    var mkdirAndCopyAndSymlink = "mkdir -p " + target + " && cp " + source + " " + target + " && mkdir -p " + path.dirname(requested) + " && ln -s " + target + "/" + path.basename(source) + " " + requested;
+    var fileListingProcess = spawn("sh", ["-c", mkdirAndCopyAndSymlink]);
+    //fileListingProcess.stdout.pipe(es.split()).pipe(es.map(function (data, cb) {
+    //  cb(null);
+    //}));
+    fileListingProcess.stdout.on('close', function(err) {
+      if (err) { reject(err); }
+      resolve();
     });
-    var wr = fs.createWriteStream(target);
-    wr.on("error", function(err) {
-      done(err);
-    });
-    wr.on("close", function(ex) {
-      done();
-    });
-    rd.pipe(wr);
-*/
   });
 };
 
@@ -327,43 +287,35 @@ var promiseToListenForHttpRequests = function() {
       });
     });
 
+
     app.use(function(req, res, next) {
-      console.log("clerk tasks here: " + req.path);
-      //var sentToCheck = shaCheckingProcess.stdin.write("/Users/mavenlink/Downloads/" + req.path + "\r\n");
-      //res.set('Content-Type', 'text/html');
-      //res.send(data);
       var requestedTildeScape = req.path.split("/")[1];
       promiseToListFolderfilesToSync().then(function(foldersToSyncAndTildeScapes) {
-        console.log(foldersToSyncAndTildeScapes);
         foldersToSyncAndTildeScapes.forEach(function(folderToSyncAndTildeScape) {
           var foundTildeScape = folderToSyncAndTildeScape[0];
           var folderToSync = folderToSyncAndTildeScape[1];
-          console.log(requestedTildeScape, foundTildeScape);
           if (foundTildeScape === requestedTildeScape) {
-            
             var checkPath = config['publicDir'] + req.path;
-
             fs.readlink(checkPath, function(err, versionPath) {
               var existingFile = path.dirname(folderToSync) + req.path;
               if (err) {
                 if ('ENOENT' === err.code) {
-                  console.log('File not found!');
-
                   fs.readFile(existingFile, function (err, data) {
                     if (err) { throw err; }
                     var checksumOfInboundFile = checksum(data, 'sha1');
                     var splitDir = splitChars(checksumOfInboundFile, 8).join("/");
                     console.log(checksumOfInboundFile, splitChars(checksumOfInboundFile, 8).join("/"));
                     promiseToCopyFile(existingFile, config['staticFileCabinetDirectory'] + "FILES/" + splitDir, checkPath).then(function() {
-                      console.log("copied");
                       res.set('Content-Type', 'text/html');
                       res.send("Copied ... reload?");
                     }).catch(function(err) {
-                      console.log(err);
+                      res.set('Content-Type', 'text/html');
+                      res.send(err);
                     });
                   });
                 } else {
-                  throw err;
+                  res.set('Content-Type', 'text/html');
+                  res.send(err);
                 }
               } else {
                 ///Users/mavenlink/workspace/static-file-cabinet/public/.sfc/FILES/293ad002/c04a8f21/a5f95d99/b077c0a4/4bb6142e
@@ -371,34 +323,40 @@ var promiseToListenForHttpRequests = function() {
                 var shaParts = shaDir.split("/");
                 shaParts.pop();
                 var shasum = shaParts.join("");
-                console.log("FILE EXISTS!!!", versionPath, shasum);
-
                 var shaCheckingProcess = spawn("shasum", ["-c", "-"]);
-
-                console.log(shasum + " " + existingFile + "\n");
                 shaCheckingProcess.stdin.write(shasum + "  " + existingFile + "\n");
-                //console.log(shaCheckingProcess.stdin);
                 shaCheckingProcess.stdin.end();
 
-                //path.dirname(folderToSync) + req.path]);
-
                 shaCheckingProcess.stdout.pipe(es.split()).pipe(es.map(function (data, cb) {
-                  console.log('sha stdout: ' + data);
-                  ///Users/mavenlink/Downloads/keep-synced/41aROjxVXbL._SY355_.jpg: OK
-                  if (data.trim().endsWith(': OK')) {
-                    var contentType = (lookup(checkPath) || 'application/octet-stream');
-                    console.log(contentType);
-                    res.set('Content-Type', contentType);
-                    fs.readFile(checkPath, function (err, data) {
-                      if (err) { throw err; }
-                      res.send(data);
-                    });
+                  ///Users/mavenlink/Downloads/keep-synced/42aROjxVXbL._SY355_.jpg: OK
+                  var trimmedLine = data.trim();
+                  if (0 != trimmedLine.length) {
+                    console.log('sha stdout: ' + trimmedLine);
+                    if (trimmedLine.endsWith(': OK')) {
+                      var contentType = (lookup(checkPath) || 'application/octet-stream');
+                      res.set('Content-Type', contentType);
+                      fs.readFile(checkPath, function (err, data) {
+                        if (err) { throw err; }
+                        res.send(data);
+                      });
+                    } else if (trimmedLine.endsWith(': FAILED')) {
+                      res.set('Content-Type', 'text/html');
+                      res.send("NEW FILE!!!: " + trimmedLine);
+                    } else {
+                      res.set('Content-Type', 'text/html');
+                      res.send("UNKNOWN: " + trimmedLine);
+                    }
                   }
                   cb();
                 }));
 
                 shaCheckingProcess.stderr.pipe(es.split()).pipe(es.map(function (data, cb) {
-                  console.log('sha stderr: ' + data);
+                  var trimmedLine = data.trim();
+                  if (0 != trimmedLine.length) {
+                    console.log("shasum error: " + trimmedLine);
+                    //res.set('Content-Type', 'text/html');
+                    //res.send("shasum error: " + data);
+                  }
                   cb();
                 }));
               }
@@ -406,7 +364,8 @@ var promiseToListenForHttpRequests = function() {
           }
         });
       }).catch(function(err) {
-        console.log(err);
+        res.set('Content-Type', 'text/html');
+        res.send(err);
       });
     });
 
@@ -416,15 +375,11 @@ var promiseToListenForHttpRequests = function() {
   });
 };
 
+
 Promise.resolve()
 .then(promiseToSetResolvedFolderfilePath)
 .then(promiseToCreateStaticFileCabinetDirectory)
 .then(promiseToListenForHttpRequests)
-//.then(promiseToListAllFilesToSync)
-//.then(function(allFiles) {
-//  console.log("allfiles", allFiles);
-//  //??? spawn('/usr/bin/open', ['http://' + config['-h'] + ':' + config['-p'] + '/']);
-//})
 .catch(function(err) {
   console.log(err);
 });
